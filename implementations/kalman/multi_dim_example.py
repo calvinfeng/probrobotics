ad50678cc8f17b64afa3df679d61ae67ce3be76e
Calvin Feng <calvinjhfeng@gmail.com>
2019-06-13 00:20:19 -0700

    Fixed bugs in multi dimensional example for Kalman filter

--- implementations/kalman/multi_dim_example.py
+++ implementations/kalman/multi_dim_example.py
@@ -1,50 +1,54 @@
-import numpy as np
+from numpy import array, dot, identity, linalg
+
 
 # State transition matrix A, acting on state
-A = np.array([
+A = array([
     [1.0, 1.0],
     [0.0, 1.0]
 ])
 
+
 # Control input matrix B, acting on control (set to identity matrix)
-B = np.array([
+B = array([
     [1.0, 0.0],
     [0.0, 1.0]
 ])
 
+
 # Measurement probability matrix
-C = np.array([
+C = array([
     [1, 0]
 ])
 
 
-def update(x, x_cov, u, u_cov):
+def predict_new_belief(x, x_cov, u, u_cov):
     """
     u_cov is the control or process noise
     """
-    mean = np.dot(A, x) + np.dot(B, u)
-    cov = np.dot(np.dot(A, x_cov), A.T) + u_cov
-    return mean, cov
+    x = dot(A, x) + dot(B, u)
+    x_cov = dot(dot(A, x_cov), A.T) + u_cov
+    return x, x_cov
 
 
-def predict(x, x_cov, z, z_cov):
-    K = np.dot(x_cov, C.T) / (np.dot(np.dot(C, x_cov), C.T) + z_cov)
-    mean = x + K * (z - C.dot(x))
-    i = np.identity(C.shape[0])
-    cov = (i - K.dot(C)).dot(x_cov)
-    return mean, cov
+def incorporate_measurement(x, x_cov, z, z_cov):
+    S = dot(dot(C, x_cov), C.T) + z_cov
+    K = dot(dot(x_cov, C.T), linalg.inv(S))
+
+    x = x + dot(K, z - C.dot(x))
+    x_cov = dot(identity(2) - dot(K, C), x_cov)
+    return x, x_cov
 
 
-def kalman_filter(x, Z, U, x_cov, z_cov, u_cov):
+def kalman_filter(x, x_cov, U, u_cov, Z, z_cov):
     """Performs Kalman filter algorithm
 
     Args:
-        x (float): Mean value of initial state
-        Z (float[]): List of measurements
-        U (float[]): List of controls
-        x_cov (float): Coariance of initial state
-        z_cov (float): Coariance of measurement
-        u_cov (float): Covariance of control
+        x (np.array): Mean value of initial state
+        x_cov (np.array): Coariance of initial state
+        U (list[np.array]): List of controls
+        u_cov (np.array): Covariance of control
+        Z (list[np.array]): List of measurements
+        z_cov (np.array): Coariance of measurement
 
     Returns:
         float: predicted mean of current state
@@ -54,45 +58,47 @@ def kalman_filter(x, Z, U, x_cov, z_cov, u_cov):
         raise ValueError("measurements and controls are not same length")
 
     for i in range(len(U)):
-        x, x_cov = update(x, x_cov, U[i], u_cov)
-        x, x_cov = predict(x, x_cov, Z[i], z_cov)
+        x, x_cov = predict_new_belief(x, x_cov, U[i], u_cov)
+        x, x_cov = incorporate_measurement(x, x_cov, Z[i], z_cov)
 
     return x, x_cov
 
 
-x = np.array([
+x = array([
     [0],
     [0],
 ])
 
-x_cov = np.array([
+x_cov = array([
     [1000.0, 0.0],
     [0.0, 1000.0],
 ])
 
-U = [
-    np.array([[0], [0]]),
-    np.array([[0], [0]]),
-    np.array([[0], [0]]),
-]
+# No external control command
+U = array([
+    [[0], [0]],
+    [[0], [0]],
+    [[0], [0]],
+])
 
-
-u_cov = np.array([
+# u_cov is usually denoted as R which represents the control covariance/uncertainty
+u_cov = array([
     [0.0, 0.0],
     [0.0, 0.0],
 ])
 
 # Mesaurement is 1, 2, 3
-Z = [
-    np.array([[1]]),
-    np.array([[2]]),
-    np.array([[3]]),
-]
+Z = array([  
+    [[1]],
+    [[2]],
+    [[3]],
+])
 
-z_cov = np.array([
+# z_cov is usually denoted as Q which represents the measurement covariance/uncertainty
+z_cov = array([
     [1],
 ])
 
-mean, cov = kalman_filter(x, Z, U, x_cov, z_cov, u_cov)
-print('mean', mean)
-print('cov', cov)
+mean, cov = kalman_filter(x, x_cov, U, u_cov, Z, z_cov)
+print('mean\n', mean)
+print('cov\n', cov)
